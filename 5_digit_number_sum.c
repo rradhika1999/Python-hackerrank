6fe8ea42ce2de2acf7e27bade39eba3b4efdc0ce
RRahulKrishnan <87916269+RRahulKrishnan@users.noreply.github.com>
2021-10-10 14:10:43 +0530

    Update on the program 5_digit_number_sum.c
    modified the program to check if the number of digits entered by the user is 5.

--- 5_digit_number_sum.c
+++ 5_digit_number_sum.c
@@ -1,19 +1,42 @@
-#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 
+int check_if_five_digits(int n)
+{
+    int digit, count = 0,flag;
+    do {
+        n /= 10;
+        ++count;
+    } while (n != 0);
+    if(count == 5)
+    {
+        flag = 1;
+    }
+    else
+    {
+        flag = 0;
+    }
+    return flag;
+}
 int main() {
 	
     int n,digit,sum=0;
+    printf("Enter a 5 digit number:");
     scanf("%d", &n);
-    //Complete the code to calculate the sum of the five digits on n.
-    while(n>0)
+    if(check_if_five_digits(n) == 1)
     {
-        digit=n%10;
-        sum=sum+digit;
-        n=n/10;
+        while(n>0)
+        {
+            digit=n%10;
+            sum=sum+digit;
+            n=n/10;
+        }
+        printf("%d",sum);
     }
-    printf("%d",sum);
+    else
+    {
+        printf("\nPlease enter a 5 digit number");
+    }
     return 0;
 }
